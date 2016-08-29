18515fb2088e682ac4a2e19f94a81ebc40009e80
SXibolet@2pitau <gdyer2@illinois.edu>
2016-08-29 03:16:36 +0000

    see log

--- controversy/app.py
+++ controversy/app.py
@@ -231,4 +231,4 @@ def pretty_date(u):
 if __name__ == '__main__':
     application.run(host='0.0.0.0',
                     port=4041,
-                    debug=True)
+                    debug=DEBUG)
